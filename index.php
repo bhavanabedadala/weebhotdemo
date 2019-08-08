8fc94ef0b021e940bb246b9102bdc371bf9573ff
bhavanabedadala <39085224+bhavanabedadala@users.noreply.github.com>
2019-08-08 14:09:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,44 @@
-<?php 
-
+ <?php 
+ 
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
-	$text = $json->result->parameters->text;
-
-	switch ($text) {
+	
+	$speech = $json->queryResult->parameters->text;
+	
+	switch ($speech) {
 		case 'hi':
-			$speech = "Hi, Nice to meet you";
+			$text = "Hi, Nice to meet you";
 			break;
-
+			
 		case 'bye':
-			$speech = "Bye, good night";
+			$text = "Bye, good night";
 			break;
-
+			
 		case 'anything':
-			$speech = "Yes, you can type anything here.";
+			$text = "Yes, you can type anything here.";
 			break;
 		
 		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			$text = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
-
+	
+	
 	$response = new \stdClass();
 	//$response->speech = $text;
 	$response->payload->google->expectUserResponse->richResponse->items[0]->simpleResponse->textToSpeech = $text;
 	//$response->source = "webhook";
-	echo json_encode($response)
-		
-// 	$response = new \stdClass();
-// 	$response->speech = $speech;
-// 	$response->displayText = $speech;
-// 	$response->source = "webhook";
-// 	echo json_encode($response);
+	echo json_encode($response);
+	
+	
 }
 else
 {
 	echo "Method not allowed";
 }
 
-?>
+?> 
